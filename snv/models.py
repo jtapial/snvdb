1fa134be2c820869594e38e97ee6972f11b0c531
Will Otterburn <willotter@gmail.com>
2014-02-03 13:58:02 +0000

    Add methods to Chain and Uniprot models

--- snv/models.py
+++ snv/models.py
@@ -22,6 +22,16 @@ class Uniprot(models.Model):
     def get_absolute_url(self):
         return reverse('uniprot-view', kwargs={'pk': self.acc_number})
 
+    def interacting_partners(self):
+        chains = self.chains.all()
+        partners = []
+        for chain in chains:
+            p = chain.binding_partners()
+            for partner in p:
+                partners.append(partner.uniprot)
+        # Return list of Uniprot objects
+        return partners
+
 class AminoAcid(models.Model):
     one_letter_code = models.CharField(max_length=1L, primary_key=True)
     three_letter_code = models.CharField(max_length=3L)
@@ -36,6 +46,81 @@ class UniprotResidue(models.Model):
     amino_acid = models.ForeignKey(AminoAcid,related_name="+")
     class Meta:
         db_table = 'uniprot_residue'
+
+class Chain(models.Model):
+    id = models.IntegerField(primary_key=True)
+    pdb_id = models.CharField(max_length=4L, blank=True)
+    pdb_chain = models.CharField(max_length=10L)
+    pdb_model = models.IntegerField()
+    seq_identity = models.CharField(max_length=10L)
+    coverage = models.CharField(max_length=10L)
+    seq_start = models.CharField(max_length=10L)
+    seq_end = models.CharField(max_length=10L)
+    uniprot = models.ForeignKey(Uniprot,db_column='uniprot_acc_number',related_name='chains')
+    class Meta:
+        db_table = 'chain'
+
+    def binding_partners(self):
+        partners = []
+        i1 = self.interactions_1.all()
+        i2 = self.interactions_2.all()
+
+        for interaction in i1:
+            partners.append(interaction.chain_2)
+        for interaction in i2:
+            partners.append(interaction.chain_1)
+
+        # Returns list of chains
+        return(partners)
+
+
+
+class InteractionType(models.Model):
+    type = models.CharField(max_length=20L, primary_key=True)
+    class Meta:
+        db_table = 'interaction_type'
+
+class Interaction(models.Model):
+    id = models.IntegerField(primary_key=True)
+    chain_1 = models.ForeignKey(Chain,db_column='chain_1_id',related_name='interactions_1')
+    chain_2 = models.ForeignKey(Chain,db_column='chain_2_id',related_name='interactions_2')
+    type = models.ForeignKey(InteractionType,db_column='type',related_name='+')
+    filename = models.CharField(max_length=100L)
+    class Meta:
+        db_table = 'interaction'
+
+class ChainResidue(models.Model):
+    id = models.IntegerField(primary_key=True)
+    chain = models.ForeignKey(Chain,db_column='chain_id',related_name='residues')
+    position = models.CharField(max_length=10L,db_column='chain_position')
+    amino_acid = models.ForeignKey(AminoAcid,related_name="+")
+    uniprot_residue = models.ManyToManyField(UniprotResidue,through='PositionMapping',related_name='chain_residues')
+    class Meta:
+        db_table = 'chain_residue'
+
+class PositionMapping(models.Model):
+    id = models.IntegerField(primary_key=True)
+    uniprot_residue = models.ForeignKey(UniprotResidue,db_column='uniprot_residue_id',related_name='+')
+    chain_residue = models.ForeignKey(ChainResidue,db_column='chain_residue_id',related_name='+')
+    class Meta:
+        db_table = 'position_mapping'
+
+class Accessibility(models.Model):
+    id = models.IntegerField(primary_key=True)
+    interaction = models.ForeignKey(Interaction,db_column='interaction_id',related_name='+')
+    chain_residues = models.ForeignKey(ChainResidue,db_column='chain_residue_id',related_name='accessibilities')
+    bound_acc = models.CharField(max_length=10L)
+    unbound_acc = models.CharField(max_length=10L)
+    disulphide_bridge_no = models.IntegerField()
+    class Meta:
+        db_table = 'accessibility'
+
+class InterfaceResidue(models.Model):
+    id = models.IntegerField(primary_key=True)
+    chain_residue = models.ForeignKey(ChainResidue,db_column='chain_residue_id',related_name='interface_residues')
+    interaction = models.ForeignKey(Interaction,db_column='interaction_id',related_name='interface_residues')
+    class Meta:
+        db_table = 'interface_residue'
 
 class SnvType(models.Model):
     type = models.CharField(max_length=20L, primary_key=True)
@@ -80,67 +165,6 @@ class SnvUniprotResidue(models.Model):
     uniprot_residue = models.ForeignKey(UniprotResidue,db_column='uniprot_residue_id')
     class Meta:
         db_table = 'snv_uniprot_residue'
-
-class Chain(models.Model):
-    id = models.IntegerField(primary_key=True)
-    pdb_id = models.CharField(max_length=4L, blank=True)
-    pdb_chain = models.CharField(max_length=10L)
-    pdb_model = models.IntegerField()
-    seq_identity = models.CharField(max_length=10L)
-    coverage = models.CharField(max_length=10L)
-    seq_start = models.CharField(max_length=10L)
-    seq_end = models.CharField(max_length=10L)
-    uniprot = models.ForeignKey(Uniprot,db_column='uniprot_acc_number',related_name='chains')
-    class Meta:
-        db_table = 'chain'
-
-class InteractionType(models.Model):
-    type = models.CharField(max_length=20L, primary_key=True)
-    class Meta:
-        db_table = 'interaction_type'
-
-class Interaction(models.Model):
-    id = models.IntegerField(primary_key=True)
-    chain_1 = models.ForeignKey(Chain,db_column='chain_1_id',related_name='+')
-    chain_2 = models.ForeignKey(Chain,db_column='chain_2_id',related_name='+')
-    type = models.ForeignKey(InteractionType,related_name='+')
-    filename = models.CharField(max_length=100L)
-    class Meta:
-        db_table = 'interaction'
-
-class ChainResidue(models.Model):
-    id = models.IntegerField(primary_key=True)
-    chain = models.ForeignKey(Chain,db_column='chain_id',related_name='residues')
-    position = models.CharField(max_length=10L,db_column='chain_position')
-    amino_acid = models.ForeignKey(AminoAcid,related_name="+")
-    uniprot_residue = models.ManyToManyField(UniprotResidue,through='PositionMapping',related_name='chain_residues')
-    class Meta:
-        db_table = 'chain_residue'
-
-class PositionMapping(models.Model):
-    id = models.IntegerField(primary_key=True)
-    uniprot_residue = models.ForeignKey(UniprotResidue,db_column='uniprot_residue_id',related_name='+')
-    chain_residue = models.ForeignKey(ChainResidue,db_column='chain_residue_id',related_name='+')
-    class Meta:
-        db_table = 'position_mapping'
-
-class Accessibility(models.Model):
-    id = models.IntegerField(primary_key=True)
-    interaction = models.ForeignKey(Interaction,db_column='interaction_id',related_name='+')
-    chain_residues = models.ForeignKey(ChainResidue,db_column='chain_residue_id',related_name='accessibilities')
-    bound_acc = models.CharField(max_length=10L)
-    unbound_acc = models.CharField(max_length=10L)
-    disulphide_bridge_no = models.IntegerField()
-    class Meta:
-        db_table = 'accessibility'
-
-class InterfaceResidue(models.Model):
-    id = models.IntegerField(primary_key=True)
-    chain_residue = models.ForeignKey(ChainResidue,db_column='chain_residue_id',related_name='interface_residues')
-    interaction = models.ForeignKey(Interaction,db_column='interaction_id',related_name='interface_residues')
-    class Meta:
-        db_table = 'interface_residue'
-
 # AUTO
 
 
